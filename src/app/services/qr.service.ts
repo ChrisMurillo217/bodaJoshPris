44472edc4ea4fe21443099c4b39c360c74e3903b
ChrisMurillo217 <camurillo43@gmail.com>
2025-05-23 10:45:07 -0500

    Add Netlify Functions for Airtable

--- src/app/services/qr.service.ts
+++ src/app/services/qr.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import * as QRCode from 'qrcode';
 
 @Injectable({providedIn: 'root'})
 export class QrService {
-  constructor(private http: HttpClient) {}
-
-  saveToAirtable(record: any) {
-    return this.http.post('/.netlify/functions/saveToAirtable', record);
-  }
-
   async generateQrCode(name: string, isMarried: boolean, partnerName?: string): Promise<string> {
     let qrData: string;
     if (isMarried && partnerName)
